54a7e5778d0bfaf7630812b3191d1e4980a1c8fe
Sahith Reddy <admin@turingitlabs.com>
2026-04-20 15:54:50 +0530

    first commit

---
+++ playwright.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+
+  reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }]
+  ],
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    }
+    // ❌ remove webkit
+  ],
+});
